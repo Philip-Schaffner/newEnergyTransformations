c0e4e40f06a9ccaeebc5ba7a4b2f6c77e614f6f1
Philip Schaffner <philip.schaffner@uzh.ch>
2015-12-18 01:22:56 +0100

    both inspections running, highlighting off

--- src/findHTTPinLoop.java
+++ src/findHTTPinLoop.java
@@ -1,13 +1,23 @@
+import com.intellij.codeInsight.highlighting.HighlightManager;
 import com.intellij.lang.java.JavaFindUsagesProvider;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.colors.EditorColors;
+import com.intellij.openapi.editor.colors.EditorColorsManager;
+import com.intellij.openapi.editor.colors.EditorColorsScheme;
+import com.intellij.openapi.editor.markup.TextAttributes;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.StatusBar;
+import com.intellij.openapi.wm.WindowManager;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.PsiClassReferenceType;
 import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.Query;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
@@ -24,114 +34,24 @@ public class findHTTPinLoop extends AnAction {
             return;
         }
         PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
-        psiFile.accept(new JavaRecursiveElementVisitor() {
-            @Override
-            public void visitReferenceExpression(PsiReferenceExpression psiReferenceExpression) {
-            }
-
-
-            /*@Override
-            public void visitBinaryExpression(PsiBinaryExpression expression) {
-                super.visitBinaryExpression(expression);
-                IElementType opSign = expression.getOperationTokenType();
-                if (opSign == JavaTokenType.EQEQ || opSign == JavaTokenType.NE) {
-                    PsiExpression lOperand = expression.getLOperand();
-                    PsiExpression rOperand = expression.getROperand();
-                    if (rOperand == null || isNullLiteral(lOperand) || isNullLiteral(rOperand)) return;
-
-                    PsiType lType = lOperand.getType();
-                    PsiType rType = rOperand.getType();
-
-                    if (isCheckedType(lType) || isCheckedType(rType)) {
-                        holder.registerProblem(expression,
-                                DESCRIPTION_TEMPLATE, myQuickFix);
-                    }
+        if (psiFile != null) {
+            JavaRecursiveElementVisitor javaRecursiveElementVisitor = new JavaRecursiveElementVisitor() {
+                @Override
+                public void visitReferenceExpression(PsiReferenceExpression psiReferenceExpression) {
                 }
-            }*/
 
-            @Override
-            public void visitAnnotation(PsiAnnotation annotation) {
-                if (annotation.getNameReferenceElement().getText().equalsIgnoreCase("POST")) {
-                    PsiElement annotatedElement = annotation.getParent().getParent();
-                    if (annotatedElement instanceof PsiMethod) {
-                        visitSuspiciousElement(annotatedElement);
-                    }
-                }
-            }
-
-            @Nullable
-            public void inspectMethodCallExpression(PsiMethodCallExpression expression) {
-                PsiExpression[] exList = expression.getArgumentList().getExpressions();
-                for (PsiExpression ex : exList) {
-                    for (PsiElement param : ex.getChildren()) {
-//                        if (getVariable(param) != null) {
-//                            PsiVariable var = getVariable(param);
-//
-                    }
-                    if (ex.getType() instanceof PsiClassReferenceType) {
-                        PsiClass aClass = ((PsiClassReferenceType) ex.getType()).resolve();
-                        aClass.getContainingFile().getVirtualFile();
-                        if ((aClass.getQualifiedName().equals("Dummy"))) {
-                            //System.out.println("found a " + aClass.getName());
-                        } else if (aClass.getQualifiedName().equals("android.location.LocationManager")) {
-                            //System.out.println("found a " + aClass.getName());
-                        }
-
-                    }
-                }
-            }
-
-            private PsiExpression getExpression(PsiCodeBlock body) {
-                final PsiStatement[] statements = body.getStatements();
-                if (statements.length == 1) {
-                    if (statements[0] instanceof PsiBlockStatement) {
-                        return getExpression(((PsiBlockStatement) statements[0]).getCodeBlock());
-                    }
-                    if (statements[0] instanceof PsiReturnStatement || statements[0] instanceof PsiExpressionStatement) {
-                        if (statements[0] instanceof PsiReturnStatement) {
-                            final PsiReturnStatement returnStatement = (PsiReturnStatement) statements[0];
-                            return returnStatement.getReturnValue();
-                        } else {
-                            final PsiExpression expression = ((PsiExpressionStatement) statements[0]).getExpression();
-                            final PsiType psiType = expression.getType();
-                            if (psiType != PsiType.VOID) {
-                                return null;
-                            }
-                            return expression;
+                @Override
+                public void visitAnnotation(PsiAnnotation annotation) {
+                    if (annotation.getNameReferenceElement().getText().equalsIgnoreCase("POST")) {
+                        PsiElement annotatedElement = annotation.getParent().getParent();
+                        if (annotatedElement instanceof PsiMethod) {
+                            visitSuspiciousElement(annotatedElement);
                         }
                     }
                 }
-                return null;
-            }
-
-            private PsiVariable getVariable(PsiElement element) {
-                if (!(element instanceof PsiJavaToken)) {
-                    return null;
-                }
-
-                final PsiJavaToken token = (PsiJavaToken) element;
-                final PsiElement parent = token.getParent();
-                if (parent instanceof PsiVariable) {
-                    return (PsiVariable) parent;
-                }
-
-
-                if (parent instanceof PsiReferenceExpression) {
-                    final PsiReferenceExpression referenceExpression = (PsiReferenceExpression) parent;
-                    final PsiElement resolvedReference = referenceExpression.resolve();
-                    if (resolvedReference instanceof PsiVariable) {
-                        return (PsiVariable) resolvedReference;
-                    }
-                }
-
-
-                if (parent instanceof PsiJavaCodeReferenceElement) {
-                    final PsiJavaCodeReferenceElement javaCodeReferenceElement = (PsiJavaCodeReferenceElement) parent;
-                    return (PsiVariable) PsiTreeUtil.getParentOfType(javaCodeReferenceElement, PsiVariable.class);
-                }
-                return null;
-            }
-        });
+            };
+            psiFile.accept(javaRecursiveElementVisitor);
+        }
     }
 
     private void visitSuspiciousElement(PsiElement annotatedElement) {
@@ -147,7 +67,8 @@ public class findHTTPinLoop extends AnAction {
                         child = child.getParent();
                     }
                     if (child instanceof PsiWhileStatement) {
-                        System.out.println("found GET in while loop");
+                        highlightElement(annotatedElement);
+                        
                     }
                 }
             }
@@ -160,5 +81,28 @@ public class findHTTPinLoop extends AnAction {
         Query<PsiReference> query = ReferencesSearch.search(element);
         Collection<PsiReference> result = query.findAll();
         return result;
+    }
+
+    private static void highlightElement(@NotNull PsiElement element)
+    {
+        final Project project = element.getProject();
+        final FileEditorManager editorManager =
+                FileEditorManager.getInstance(project);
+        final HighlightManager highlightManager =
+                HighlightManager.getInstance(project);
+        final EditorColorsManager editorColorsManager =
+                EditorColorsManager.getInstance();
+        final Editor editor = editorManager.getSelectedTextEditor();
+        final EditorColorsScheme globalScheme =
+                editorColorsManager.getGlobalScheme();
+        final TextAttributes textattributes =
+                globalScheme.getAttributes(
+                        EditorColors.SEARCH_RESULT_ATTRIBUTES);
+        final PsiElement[] elements = new PsiElement[]{element};
+        highlightManager.addOccurrenceHighlights(
+                editor, elements, textattributes, true, null);
+        final WindowManager windowManager = WindowManager.getInstance();
+        final StatusBar statusBar = windowManager.getStatusBar(project);
+        statusBar.setInfo("Press Esc to remove highlighting");
     }
 }
